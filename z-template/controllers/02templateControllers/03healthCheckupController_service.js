9646ad6b9cf14fd4348079fc6c96a1025541b976
xiongshuqiu <258752593@qq.com>
2025-02-16 13:41:11 +0300

    77

--- z-template/controllers/02templateControllers/03healthCheckupController_service.js
+++ z-template/controllers/02templateControllers/03healthCheckupController_service.js
@@ -57,6 +57,36 @@ const getAllHealthCheckups = async (req, res) => {
         },
       ]);
       query = { elderlyId: { $in: query.map((item) => item.elderlyId) } };
+      // } else if (role === 'medical') {
+      //   query = await User.aggregate([
+      //     {
+      //       $match: { _id: new mongoose.Types.ObjectId(_id) }, // 使用 new 关键字实例化 ObjectId
+      //     },
+      //     {
+      //       $lookup: {
+      //         from: 'employees',
+      //         localField: 'userId',
+      //         foreignField: 'userId',
+      //         as: 'employeeDetails',
+      //       },
+      //     },
+      //     { $unwind: '$employeeDetails' },
+      //     {
+      //       $lookup: {
+      //         from: 'elderlies',
+      //         localField: 'employeeDetails.employeeId',
+      //         foreignField: 'employeeId',
+      //         as: 'elderlyDetails',
+      //       },
+      //     },
+      //     { $unwind: '$elderlyDetails' },
+      //     {
+      //       $project: {
+      //         elderlyId: '$elderlyDetails.elderlyId',
+      //       },
+      //     },
+      //   ]);
+      //   query = { elderlyId: { $in: query.map((item) => item.elderlyId) } };
     } else if (role === 'admin' || role === 'medical') {
       query = {}; // 管理员可以查看所有数据
     } else {
