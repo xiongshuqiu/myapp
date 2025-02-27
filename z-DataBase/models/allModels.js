6e44736ca38862f4c35975365d694a2726132b63
xiongshuqiu <258752593@qq.com>
2025-02-27 11:14:22 +0300

    108

--- z-DataBase/models/allModels.js
+++ z-DataBase/models/allModels.js
@@ -33,7 +33,7 @@ const elderlyResidentSchema = new mongoose.Schema({
   residentId: { type: String, required: true, unique: true }, // 住宿编号 R001
   elderlyId: { type: String, ref: 'Elderly', required: true }, // 老人引用 E001
   checkInTime: { type: Date, required: true }, // 入住时间
-  checkOutTime: { type: Date }, // 退住时间
+  checkOutTime: { type: Date, default: null },// 退住时间
   status: { type: String, required: true }, // 状态: Active/Inactive
 });
 
