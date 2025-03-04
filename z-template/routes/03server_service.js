8644c151e6ab1f673789bdad8af52dd9e3e0642e
xiongshuqiu <258752593@qq.com>
2025-03-04 11:56:15 +0300

    Remove z-template folder and update .gitignore

--- z-template/routes/03server_service.js
+++
@@ -1,9 +0,0 @@
- // 导入app.js中的Express应用实例
- const app = require('./app');
- // 从环境变量中读取端口号
- const PORT = process.env.PORT;
-// 启动Express服务器并监听指定端口
-app.listen(PORT, () => {
-    console.log(`user-service running on  port ${PORT}`);
-  });
-   
